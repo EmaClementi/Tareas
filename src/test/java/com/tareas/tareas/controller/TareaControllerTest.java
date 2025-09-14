60b4ce84b0f28df696446b22fc7609fd43e5874e
EmaClementi <emicabj4221@gmail.com>
2025-09-14 14:22:20 -0300

    creando test unitarios de buscar tarea inexistente, modificar tarea existente e inexistente

--- src/test/java/com/tareas/tareas/controller/TareaControllerTest.java
+++ src/test/java/com/tareas/tareas/controller/TareaControllerTest.java
@@ -1,6 +1,10 @@
 package com.tareas.tareas.controller;
 
 import com.tareas.tareas.domain.tarea.*;
+import com.tareas.tareas.domain.usuario.Role;
+import com.tareas.tareas.domain.usuario.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +19,11 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 //Un test sigue la regla AAA (Arrange, Act, Assert):
 //Arrange (Given) → preparás los datos.
@@ -41,8 +43,14 @@ public class TareaControllerTest {
     @Autowired
     private JacksonTester<DatosRespuestaTarea> datosRespuestaTareaJacksonTester;
 
+    @Autowired
+    private JacksonTester<DatosActualizarTarea> datosActualizarTareaJacksonTester;
+
     @MockBean
     private TareaService tareaService;
+
+    @Autowired
+    private EntityManager em;
 
 
     @Test // con esta anotacion digo que el metodo es una prueba unitaria
@@ -85,4 +93,65 @@ public class TareaControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
+
+    @Test
+    @DisplayName("Buscamos una tarea que existe y deberia devolver un 200 ok")
+    @WithMockUser
+    void buscarTareaExistente() throws Exception {
+        var usuario = new Usuario(1L,"ema","clementi","emi@gmail.com","123456", Role.USER,null);
+        DatosCrearTarea nuevaTarea = new DatosCrearTarea("Ir al medico", "Ir al medico a controlar resultados", LocalDateTime.now(),LocalDateTime.now().plusHours(2),Estado.PENDIENTE, Importancia.ALTA, 1L);
+        var tarea = new Tarea(nuevaTarea,usuario);
+        var respuestaTarea = new DatosRespuestaTarea(tarea);
+        when(tareaService.buscarTareaPorId(1L)).thenReturn(respuestaTarea); // este metodo hace que cuando alguien llame al metodo de buscarTarea, devuelva automaticamente
+        // el dto de respuesta, esto evita que se llame a la base de datos real
+
+
+        var response = mvc.perform(get("/tareas/1")) // uso mockmvc para simular la solicitud al endpoint
+                .andReturn()
+                .getResponse(); // obtengo la respuesta que devolveria el servidor
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value()); // verifico que la respuesta http sea un 200 ok
+        assertThat(response.getContentAsString()) // convierto el body de la respuesta en un string
+                .isEqualTo(datosRespuestaTareaJacksonTester.write(respuestaTarea).getJson());
+        // convierto el dto de la respuesta en json en formato string, y lo comparo con el que recibi, si son iguales, el test pasa
+
+    }
+    @Test
+    @DisplayName("Actualizamos una tarea y deberia devolver un 200 ok con los datos de la tarea")
+    @WithMockUser
+    void modificarTarea() throws Exception {
+        DatosActualizarTarea actualizarTarea = new DatosActualizarTarea(1L, "Ir al medico", "Ir al medico a controlar resultados", LocalDateTime.now(),LocalDateTime.now().plusHours(2),Estado.PENDIENTE, Importancia.ALTA, 1L);
+        var respuestaTarea = new DatosRespuestaTarea(actualizarTarea.nombre(),actualizarTarea.descripcion(),actualizarTarea.estado(),actualizarTarea.importancia());
+
+        when(tareaService.editarTarea(actualizarTarea)).thenReturn(respuestaTarea); // cuando el service reciba el dto actualizarTarea, debe devolver el dto respuestaTarea
+
+        var response = mvc.perform(put("/tareas")
+                .contentType(MediaType.APPLICATION_JSON) // decimos que el contenido va a venir en el cuerpo de la solicitud
+                .content(datosActualizarTareaJacksonTester.write(actualizarTarea).getJson()))// convertimos el objeto en json con jackson
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString())
+                .isEqualTo(datosRespuestaTareaJacksonTester.write(respuestaTarea).getJson());
+    }
+
+    @Test
+    @DisplayName("Actualizamos una tarea que no existe, deberia devolver un 404 not found")
+    @WithMockUser
+    void modificarTareaInexistente() throws Exception {
+        DatosActualizarTarea actualizarTarea = new DatosActualizarTarea(99L, "Ir al medico", "Ir al medico a controlar resultados", LocalDateTime.now(),LocalDateTime.now().plusHours(2),Estado.PENDIENTE, Importancia.ALTA, 1L);
+
+        when(tareaService.editarTarea(actualizarTarea)).thenThrow(new EntityNotFoundException("Tarea no encontrada"));
+
+
+        var response = mvc.perform(put("/tareas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(datosActualizarTareaJacksonTester.write(actualizarTarea).getJson()))
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
 }
